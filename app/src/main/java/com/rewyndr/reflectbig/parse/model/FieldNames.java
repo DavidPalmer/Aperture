06855988999ad9aa6d81f24004227822e3500028
Satish <satishr@sase.ssn.edu.in>
2014-07-16 02:35:43 -0400

    Updated comments

---
+++ app/src/main/java/com/rewyndr/reflectbig/parse/model/FieldNames.java
@@ -0,0 +1,13 @@
+package com.rewyndr.reflectbig.parse.model;
+
+/**
+ * This provides the names of fields in Parse
+ *
+ * Created by Satish on 7/16/2014.
+ */
+public class FieldNames {
+
+    public static final String PHOTO_NO = "photoNo";
+
+    public static final String PHOTO_FILE = "photoFile";
+}
