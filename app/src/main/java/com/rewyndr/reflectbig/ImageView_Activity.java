fcc0b86045ffa240324de800c91f21de2d0ff867
rajaramanr <rajaraman.raghunathan@gmail.com>
2014-07-16 02:32:47 -0400

    Image view
    Slider working

--- app/src/main/java/com/rewyndr/reflectbig/ImageView_Activity.java
+++ app/src/main/java/com/rewyndr/reflectbig/ImageView_Activity.java
@@ -1,74 +1,58 @@
 package com.rewyndr.reflectbig;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class ImageView_Activity extends Activity {
-    private static final int SELECT_PICTURE = 1;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view_);
-        displayPicture();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ImagePagerAdapter adapter = new ImagePagerAdapter();
+        viewPager.setAdapter(adapter);
     }
 
-    private String selectedImagePath;
-    private ImageView img;
+    private class ImagePagerAdapter extends PagerAdapter {
+        private int[] mImages = new int[] {
+                R.drawable.chiang_mai,
+                R.drawable.himeji,
+                R.drawable.petronas_twin_tower,
+                R.drawable.ulm,
+                R.drawable.ra,
+                R.drawable.raa
+        };
 
-    private void displayPicture() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        getInputForImageView(intent);
-    }
-
-    private void getInputForImageView(Intent intent) {
-        img = (ImageView)findViewById(R.id.imageView);
-        startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
-                System.out.println("Image Path : " + selectedImagePath);
-                img.setImageURI(selectedImageUri);
-            }
+        @Override
+        public int getCount() {
+            return mImages.length;
         }
-    }
 
-    public String getPath(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.image_view_, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((ImageView) object);
         }
-        return super.onOptionsItemSelected(item);
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Context context = ImageView_Activity.this;
+            ImageView imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            imageView.setImageResource(mImages[position]);
+            ((ViewPager) container).addView(imageView, 0);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView((ImageView) object);
+        }
     }
 }
