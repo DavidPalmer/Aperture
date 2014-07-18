4fd78022ca90d4d8ef1011ca4464f6eada92483b
Satish <satishr@sase.ssn.edu.in>
2014-07-18 17:16:12 -0400

    Changed method signature for ViewPhoto API
    Removed separate methods for different sizes and added single method
    with parameter for photo size

--- app/src/androidTest/java/com/rewyndr/reflectbig/ViewPhotoParseUnitTest.java
+++ app/src/androidTest/java/com/rewyndr/reflectbig/ViewPhotoParseUnitTest.java
@@ -2,6 +2,7 @@ package com.rewyndr.reflectbig;
 
 import android.test.AndroidTestCase;
 
+import com.rewyndr.reflectbig.common.PhotoType;
 import com.rewyndr.reflectbig.parse.impl.ViewPhotoParse;
 
 import java.util.List;
@@ -19,7 +20,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhotos() {
         try {
-            List<String> results = instance.getPhotos(1, 10);
+            List<String> results = instance.getPhotos(1, 10, PhotoType.ACTUAL);
             assertNotNull("Results cannot be null", results);
             assertEquals("Results size must match input size", 10, results.size());
         } catch (Exception e) {
@@ -29,7 +30,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhoto() {
         try {
-            String result = instance.getPhoto(10);
+            String result = instance.getPhoto(10, PhotoType.ACTUAL);
             assertNotNull("Results cannot be null", result);
             assertEquals("Url not matching", "http://files.parsetfss.com/415b86f6-7316-4359-a922-11a1d4d2f96f/tfss-d045e7d6-ec3f-4541-b084-ba172c535622-file0001376718168.jpg", result);
         } catch (Exception e) {
@@ -39,7 +40,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhotosThumbnail() {
         try {
-            List<String> results = instance.getPhotosThumbnail(11, 20);
+            List<String> results = instance.getPhotos(11, 20, PhotoType.THUMBNAIL);
             assertNotNull("Results cannot be null", results);
             assertEquals("Results size must match input size", 10, results.size());
         } catch (Exception e) {
@@ -49,7 +50,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhotoThumbnail() {
         try {
-            String result = instance.getPhotoThumbnail(15);
+            String result = instance.getPhoto(15, PhotoType.THUMBNAIL);
             assertNotNull("Results cannot be null", result);
             assertEquals("Url not matching", "http://files.parsetfss.com/415b86f6-7316-4359-a922-11a1d4d2f96f/tfss-a35113dd-62c9-4d50-b320-3efb5d6d7ea9-file0001608482449_640.jpg", result);
         } catch (Exception e) {
@@ -59,7 +60,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhotosSmall() {
         try {
-            List<String> results = instance.getPhotosSmall(21, 30);
+            List<String> results = instance.getPhotos(21, 30, PhotoType.SMALL);
             assertNotNull("Results cannot be null", results);
             assertEquals("Results size must match input size", 10, results.size());
         } catch (Exception e) {
@@ -69,7 +70,7 @@ public class ViewPhotoParseUnitTest extends AndroidTestCase {
 
     public void testGetPhotoSmall() {
         try {
-            String result = instance.getPhotoSmall(100);
+            String result = instance.getPhoto(100, PhotoType.SMALL);
             assertNotNull("Results cannot be null", result);
             assertEquals("Url not matching", "http://files.parsetfss.com/415b86f6-7316-4359-a922-11a1d4d2f96f/tfss-b529add3-efe0-47e6-a161-c889d273c759-ch4_convent_1024.jpg", result);
         } catch (Exception e) {
