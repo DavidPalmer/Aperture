33b9d0a379185d6441d15f6dd4e1e28618754c7a
rajaramanr <rajaraman.raghunathan@gmail.com>
2014-09-06 23:48:10 -0400

    Fixed #3 issue
    This has complete fix for the issue including the arbitrary conditions
    like force stop app

--- app/src/main/java/com/rewyndr/reflectbig/common/PreferenceConstants.java
+++ app/src/main/java/com/rewyndr/reflectbig/common/PreferenceConstants.java
@@ -7,4 +7,6 @@ public class PreferenceConstants {
     public static final String PREFERENCE_NAME = "REFLECT_BIG";
     public static final String LOGIN_EMAIL_ID = "EMAIL";
     public static final String LOGIN_PASSWORD = "PASSWORD";
+    public static final String CURRENT_POSITION = "CURRENT_POSITION";
+    public static final String WAS_PAUSED = "WAS_PAUSED";
 }
