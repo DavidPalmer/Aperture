b523ca6c892d88bbc017235172ef4b7317c0e48b
rajaramanr <rajaraman.raghunathan@gmail.com>
2014-10-13 11:59:09 -0400

    Bug fixes and Invitation screen validations
    #28,  #25

--- app/src/main/java/com/rewyndr/reflectbig/common/Constants.java
+++ app/src/main/java/com/rewyndr/reflectbig/common/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
     public static final int IMAGE_START_ID = 1;
     public static final int FETCH_LENGTH = 30;
     public static final String YES = "Y";
+    public static final String EMPTY_STRING = "";
+    public static final String DATE_DELIMITER = " - ";
+    public static final String TIME_DELIMITER = " : ";
+    public static final String CREATE_SCREEN_ERROR_MSG = "Cannot move back to create event";
 }
